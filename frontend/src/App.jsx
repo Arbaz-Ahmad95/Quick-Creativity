d4b985ef16eb82e198e2494ac10b0fceee2eb8b4
Arbaz-Ahmad95 <arbazahmad363@gmail.com>
2025-12-27 00:20:03 +0530

    service page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Services from './components/Services'
-import Team from './components/Team'
-import WhyTrustUs from './components/WhyTrustUs'
-import Hero from "./components/Hero"
-import Footer from './components/Footer'
+import ServicesSection from './Pages/ServicesSection'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <Services/>
-      <Team/>
-      <WhyTrustUs/>
-      <Footer/>
+     
+       <ServicesSection/>
     </div>
   )
 }
